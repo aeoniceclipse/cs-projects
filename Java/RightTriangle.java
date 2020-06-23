caba096a93ca3447c1262c8e62aac6410656ba26
aeoniceclipse <aeoniceclipse@users.noreply.github.com>
2020-06-22 20:18:19 -0400

    220620 small update

--- Java/RightTriangle.java
+++ Java/RightTriangle.java
@@ -5,7 +5,7 @@ public class RightTriangle {
 		int c = Integer.parseInt(args[2]);
 		boolean tri;
 
-		tri = (a > 0) && (b > 0) && (c > 0);
+		tri = (a >= 0) && (b >= 0) && (c >= 0);
 		tri = tri && ((a * a) + (b * b) == (c * c));
 		tri = tri || ((a * a) + (c * c) == (b * b));
 		tri = tri || ((b * b) + (c * c) == (a * a));
